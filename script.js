ef6c7ea3422f6c69607482d9c1cdd2e9242a052d
fikserr <azizxon7777777@gmail.com>
2024-01-05 17:58:21 +0500

    load

---
+++ script.js
@@ -0,0 +1,21 @@
+// let son1 = prompt("Birinchi raqamni kiritng");
+
+// let son2 = prompt("Ikkinchi raqamni kiritng");
+
+// let son3 = prompt("Uchinchi raqamni kiritng");
+
+
+
+// if(son1 < son2 || son1 > son2 || son1 < son3 || son1 > son3){
+//     console.log("Sizning kiritgan o`rta soningiz");
+// }else{
+//     console.log("Siz notugri son kiritdingiz");
+// }
+
+
+let fraktal = prompt("Son kiriting");
+
+for (let i = 0; i < fraktal; i++){
+    let a = i < 2;
+    console.log(a);
+}
